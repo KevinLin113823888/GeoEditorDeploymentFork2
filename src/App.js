19157cf2f4599cf929d4bd593d0a9719d9c1f251
daoqingao <daoqingao@gmail.com --git config --global user.email daoqingao@gmail.com>
2023-03-24 23:57:01 -0400

    init

--- src/App.js
+++ src/App.js
@@ -100,7 +100,7 @@ function App() {
         <div className="App">
             <button onClick={upload}>
                 Display North America geojson
-                come on
+                come ona
             </button>
 
             <div>
