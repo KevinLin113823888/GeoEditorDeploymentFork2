ee7bd6f83d36dcba3115a4cb5cddea9a27978857
Kevin Lin <kevin.lin.13@stonybrook.edu>
2023-03-31 19:48:42 -0400

    Axios

---
+++ client/src/store/store-request-api/index.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+axios.defaults.withCredentials = true;
+const api = axios.create({
+    baseURL: 'http://localhost:9000/',
+})
+
+export const createMap = (title,newMapCardData,mapData) => {
+    return api.post(`/map/`, {
+        // SPECIFY THE PAYLOAD
+        title:title,
+        mapCardData: newMapCardData,
+        mapData: mapData
+    })
+}
+export const updateMapById = (id,updatedMap, updatedMapData, updatedMapCard) => {
+    return api.post(`/map/${id}`, {
+        // SPECIFY THE PAYLOAD
+        updatedMap:updatedMap,
+        updatedMapData: updatedMapData,
+        updatedMapCard: updatedMapCard
+    })
+}
+const apis = {
+    createMap,
+    updateMapById
+}
+
+export default apis
