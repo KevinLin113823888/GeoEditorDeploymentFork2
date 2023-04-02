d8abc7bb99fe11f24d8dc04f9d02ae8477632864
daoqingao <daoqingao@gmail.com --git config --global user.email daoqingao@gmail.com>
2023-04-02 00:58:05 -0400

    tests that are hooked up to the mongodb.... might not be good if you run too many tests

--- server/tests/user.test.js
+++ server/tests/user.test.js
@@ -1,26 +1,82 @@
 const app = require('../index.js');
 const request = require('supertest');
-
+const mongoose = require("mongoose");
 
 //TODO: .... mongodb needs to be mocked because all the tests are actual api requests....
 
-test("register", async () => {
-    const response =  await request(app).post("/user/register").send({
-        name: "username",
-        username: "password",
-        email: "mockEmailuhh",
-        password: "mockPassword"
+describe('Testing user controller', () => {
+    const mockCredentials = new mongoose.Types.ObjectId().toString()
+    console.log(mockCredentials)
 
+    test("register", async () => {
+        const response =  await request(app).post("/user/register").send({
+            name: mockCredentials,
+            username: mockCredentials,
+            email: mockCredentials,
+            password: mockCredentials
+        })
+        console.log(response)
+        expect(response.statusCode).toBe(200)
     })
-    console.log(response)
-    expect(response.statusCode).toBe(200)
-})
-// describe('Testing user controller', () => {
-//     // it("exposes the API endpoint", async () => {
-//     //     await request(server).post("/user").expect(200, { message: "Hello, world!" });
-//     // });
-//
-//
-//
-// });
-//
+
+    test("login", async () => {
+        const response =  await request(app).post("/user/login").send({
+            // name: mockCredentials,
+            username: mockCredentials,
+            // email: mockCredentials,
+            password: mockCredentials
+        })
+        console.log(response)
+        expect(response.statusCode).toBe(200)
+    })
+
+    test("logout", async () => {
+        const response =  await request(app).post("/user/logout").send({
+            // name: mockCredentials,
+            // username: mockCredentials,
+            // email: mockCredentials,
+            // password: mockCredentials
+        })
+        console.log(response)
+        expect(response.statusCode).toBe(200)
+    })
+
+    test("forgotUsername", async () => {
+        const response =  await request(app).post("/user/forgotUsername").send({
+            // name: mockCredentials,
+            // username: mockCredentials,
+            email: mockCredentials,
+            // password: mockCredentials
+        })
+        console.log(response)
+        expect(response.statusCode).toBe(200)
+    })
+
+    test("sendPasswordRecoveryCode", async () => {
+        const response =  await request(app).post("/user/sendPasswordRecoveryCode").send({
+            // name: mockCredentials,
+            // username: mockCredentials,
+            email: mockCredentials,
+            // password: mockCredentials
+        })
+        console.log(response)
+        expect(response.statusCode).toBe(200)
+    })
+
+    test("changePassword", async () => {
+        const response =  await request(app).post("/user/changePassword").send({
+            // name: mockCredentials,
+            // username: mockCredentials,
+            // email: mockCredentials,
+            // password: mockCredentials
+        })
+        console.log(response)
+        expect(response.statusCode).toBe(200)
+    })
+
+
+});
+
+
+
+
