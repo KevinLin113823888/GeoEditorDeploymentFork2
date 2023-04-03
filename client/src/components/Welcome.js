3b75cbe7ba5b1c8ef16ddbae00eb1915fac934a7
daoqingao <daoqingao@gmail.com --git config --global user.email daoqingao@gmail.com>
2023-04-02 22:14:26 -0400

    testing changes to geoEditor

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom';
 function Welcome() {
     return (
         <div className="Welcome">
-            <h1>Welcome to GeoEditor!!!!!!!!!!!</h1>
+            <h1>Welcome to GeoEditor</h1>
+            <h1>Test things</h1>
+
             <nav>
                 <Link data-cy="home-link" to="/">Home</Link>
                 <Link data-cy="register-link" to="/register">Register</Link>
