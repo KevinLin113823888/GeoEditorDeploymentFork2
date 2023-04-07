133bad4be2bd3937c176ee674be338f6c5e34eb6
DavidWang621 <davidwang759@gmail.com>
2023-04-07 17:30:29 -0400

    map delete id controller

--- server/controllers/mapController.js
+++ server/controllers/mapController.js
@@ -1,3 +1,4 @@
+var mongoose = require('mongoose');
 const Map = require('../models/mapModel')
 const User = require('../models/userInfoModel');
 const MapCard = require('../models/mapCardModel')
@@ -6,54 +7,58 @@ const CommunityPreview = require('../models/communityPreviewModel')
 
 class mapController {
     static async createMap(req, res) {
-        // const body = req.body;
-        // console.log("createMap body: " + JSON.stringify(body));
-    
-        // if (!body) {
-        //     return res.status(400).json({
-        //         success: false,
-        //         error: 'You must provide a Map',
-        //     })
-        // }
-        // console.log(body.title)
-        // const map = new Map();
-        // if (!map) {
-        //     return res.status(400).json({ success: false, error: err })
-        // }
-
         try{
-            
             var { title } = req.body;
             let session = req.cookies.values;
             var owner = await User.findOne({username: session.username});
             var newMap = new Map({
                 title: title,
                 owner: owner._id
-            })
-            
+            });
             
             await newMap.save();
 
-            console.log(title)
+            console.log(title);
             
             var newMapCard = new MapCard({
                 title: title,
                 map: newMap._id
             })
             
-            await newMapCard.save()
+            await newMapCard.save();
             
             owner.ownedMaps.push(newMap._id);
             
             owner.ownedMapCards.push(newMapCard._id);
 
             await owner.save()
-            console.log(newMap._id.toString())
-            return res.status(200).json({status: 'OK', title: title, mapId: newMap._id.toString() });
+            console.log(newMap._id.toString());
+            return res.status(200).json({status: 'OK', title: title, mapId: newMap._id.toString()});
         }
         catch(e){
             return res.status(400).json({error: true, message: e.toString() });
         }
+    }
+
+    static async deleteMapById(req, res) {
+        var { id } = req.body;
+
+        var id = mongoose.Types.ObjectId(id);
+        var mapCard = MapCard.findOneAndDelete({ _id: id });
+        var map = Map.findOneAndDelete({ _id: mapCard._id });
+        var mapData = MapData.findOneAndDelete({ _id: map._id });
+
+        User.findOneAndUpdate({_id: id}, { $pull: {ownedMapCards: mapCards._id, ownedMaps: map._id} });
+
+        return res.status(400).json({status: 'OK'});
+    }
+
+    static async duplicateMapById(req, res) {
+
+    }
+
+    static async changeMapNameById(req, res) {
+        
     }
 }
 
--- server/routes/mapRoute.js
+++ server/routes/mapRoute.js
@@ -4,5 +4,8 @@ const router = express.Router()
 const MapController = require('../controllers/mapController')
 
 router.post('/createMap', MapController.createMap);
+router.post('/deleteMapById', MapController.deleteMapById);
+router.post('/duplicateMapById', MapController.duplicateMapById);
+router.post('/changeMapNameById', MapController.changeMapNameById);
 
 module.exports = router
