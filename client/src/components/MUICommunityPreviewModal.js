4030c27b75f8e85b594890c78a9085222b3d4fd1
Kevin Lin <kevin.lin.13@stonybrook.edu>
2023-04-07 21:20:28 -0400

    Properties CSS

--- client/src/components/MUICommunityPreviewModal.js
+++ client/src/components/MUICommunityPreviewModal.js
@@ -24,7 +24,7 @@ const style = {
     left: '50%',
     transform: 'translate(-50%, -50%)',
     width: '85%',
-    height: '83%',
+    height: '87%',
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
