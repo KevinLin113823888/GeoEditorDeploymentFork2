0f7b482a40cd1603bcc879572910802bd50294db
DavidWang621 <davidwang759@gmail.com>
2023-04-14 17:02:22 -0400

    can add map

--- server/routes/mapRoute.js
+++ server/routes/mapRoute.js
@@ -3,12 +3,13 @@ const router = express.Router()
 //const auth = require('../auth')
 const MapController = require('../controllers/mapController')
 
-router.get('/createMap', MapController.createMap);
+router.post('/createMap', MapController.createMap);
 // router.post('/getMapById', MapController.getMapById);
 // router.post('/deleteMapById', MapController.deleteMapById);
 router.post('/duplicateMapById', MapController.duplicateMapById);
 router.post('/changeMapNameById', MapController.changeMapNameById);
 router.post('/publishMapById', MapController.publishMapById);
 router.post('/mapClassificationById', MapController.mapClassificationById);
+router.post('/saveMapById', MapController.saveMapById);
 
 module.exports = router
