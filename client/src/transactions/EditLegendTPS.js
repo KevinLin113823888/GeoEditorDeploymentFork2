4d02e91338ec3e0c4ea55c3f1ce0417fbb417ce8
daoqingao <daoqingao@gmail.com --git config --global user.email daoqingao@gmail.com>
2023-04-18 22:00:49 -0400

    trying to try out diff stuff

--- client/src/transactions/EditLegendTPS.js
+++ client/src/transactions/EditLegendTPS.js
@@ -20,6 +20,7 @@ export default class EditLegendTPS extends jsTPS_Transaction {
 
         this.oldColor = mappedData.oldColor
         this.newColor = mappedData.newColor
+        this.diff =  require('jsondiffpatch')
     }
 
     //check if the geojson contains graphicalData key and adds it if not
@@ -27,7 +28,7 @@ export default class EditLegendTPS extends jsTPS_Transaction {
         //assign value only if undefined (does not exist)
         // console.log("called to init")
         geoJsonObj.graphicalData ??= {}
-        geoJsonObj.graphicalData.backgroundColor ??= ""
+        geoJsonObj.graphicalData.backgroundColor ??= "#FFFFFF"
         geoJsonObj.graphicalData.textBox ??= []
         geoJsonObj.graphicalData.legend ??= []
     }
@@ -48,11 +49,17 @@ export default class EditLegendTPS extends jsTPS_Transaction {
         console.log("transac")
         if(this.type === "add"){
             console.log("add transact")
-            mapObj.graphicalData.legend.splice(0,0,{
-                color: "#000000",
-                legendText: "Default text",
-            })
-            this.oldIndex = 0
+            // let before = JSON.parse(JSON.stringify(mapObj.graphicalData))
+            // mapObj.graphicalData.legend.splice(0,0,{
+            //     color: "#000000",
+            //     legendText: "Default text",
+            // })
+            // this.oldIndex = 0
+
+
+            this.diffObjDelta = {"legend":{"0":[{"color":"#000000","legendText":"Default text"}],"_t":"a"}}
+            this.diff.patch(mapObj.graphicalData,this.diffObjDelta)
+
         }
         else if (this.type === "edit"){
             console.log("called to edit right here")
@@ -79,7 +86,8 @@ export default class EditLegendTPS extends jsTPS_Transaction {
     undoTransaction() {
     let mapObj = this.mapObj
     if(this.type === "add"){
-        mapObj.graphicalData.legend.splice(this.oldIndex,1)
+        // mapObj.graphicalData.legend.splice(this.oldIndex,1)
+        this.diff.unpatch(mapObj.graphicalData,this.diffObjDelta)
         }
     else if (this.type === "edit"){
         mapObj.graphicalData.legend[this.oldIndex].legendText  = this.oldText
