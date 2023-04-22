f45d02bce4e0aa997b577b3027dd1c89b1b45a87
daoqingao <daoqingao@gmail.com --git config --global user.email daoqingao@gmail.com>
2023-04-21 20:10:04 -0400

    jest test

---
+++ client/tests/TextBox.test.js
@@ -0,0 +1,22 @@
+
+const geo = require('./testGeoJson.json');
+
+describe('Testing textbox jstsp', () => {
+    beforeAll(() => {
+        console.log(geo)
+        console.log("idk whats this")
+    })
+    afterAll(() => {
+
+    })
+
+    test("add", async () => {
+
+    })
+
+
+});
+
+
+
+
