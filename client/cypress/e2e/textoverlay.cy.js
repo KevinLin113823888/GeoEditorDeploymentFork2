edc01ac8f3bca0b11701d663d6b6138702aebdb9
Sheng Lu <sheng.lu@stonybrook.edu>
2023-04-24 12:22:15 -0400

    cypress test fix?

--- client/cypress/e2e/textoverlay.cy.js
+++ client/cypress/e2e/textoverlay.cy.js
@@ -1,27 +1,22 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://bejewelled-palmier-26e86d.netlify.app/')
+    cy.visit('https://bejewelled-palmier-26e86d.netlify.app/');
+
     /* ==== Generated with Cypress Studio ==== */
     cy.get('[data-cy="login-link"]').click();
     cy.get('#userName').clear();
     cy.get('#userName').type('test');
-    cy.get('[data-cy="password-input"]').click();
-    cy.get('#password').clear();
+    cy.get('#password').clear('t');
     cy.get('#password').type('test');
     cy.get('[data-cy="submit-button"]').click();
-
-    cy.get('#cardImage644683a310eedf34e440433d').click();
-
-    // cy.get('#leaflet-tooltip-2485 > input', { timeout: 20000 }).dblclick();
-    // cy.get('#leaflet-tooltip-2485 > input').clear();
-    // cy.get('#leaflet-tooltip-2485 > input').type('Russiaaaaa');
-
-    // cy.get('.MuiGrid-grid-xs-8').click();
-    // cy.get('[title="addText"] > .leaflet-buttons-control-button > .control-icon').click();
-    // cy.get('#mapitem').click();
-    // cy.get('.active > .leaflet-pm-actions-container > .leaflet-pm-action').click();
-    // cy.get('[title="undo"] > .leaflet-buttons-control-button > .control-icon').click();
-    // cy.get('.active > .leaflet-buttons-control-button > .control-icon').click();
-    /* ==== End Cypress Studio ==== */
+    cy.get('#cardImage6446ab663b7d0ddb8ecd43c3').click();
+    cy.get('#leaflet-tooltip-259').dblclick();
+    cy.get('#leaflet-tooltip-259 > input').clear();
+    cy.get('#leaflet-tooltip-259 > input').type('Russiaaaaa');
+    cy.get('.MuiGrid-grid-xs-8').click();
+    cy.get('[title="addText"] > .leaflet-buttons-control-button > .control-icon').click();
+    cy.get('#mapitem').click();
+    cy.get('.active > .leaflet-pm-actions-container > .leaflet-pm-action').click();
+    cy.get('[title="undo"] > .leaflet-buttons-control-button > .control-icon').click();
   })
 })
