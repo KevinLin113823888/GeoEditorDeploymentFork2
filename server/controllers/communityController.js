c507c940e6e8ffc5a72088f7a47924eff5125795
DavidWang621 <davidwang759@gmail.com>
2023-04-24 08:41:19 -0400

    debugging block

--- server/controllers/communityController.js
+++ server/controllers/communityController.js
@@ -241,6 +241,7 @@ class communityController {
         try {
             var { id } = req.body;
             let username = req.cookies.values.username;
+            console.log("ID", id, username);
 
             var currentCommunityPreview = await CommunityPreview.findOne({ _id: new mongoose.Types.ObjectId(id) });
             var currentCommunityCard = await MapCard.findOne({ _id: new mongoose.Types.ObjectId(currentCommunityPreview.mapCard) });
